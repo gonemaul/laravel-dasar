15c02e9fd6a4e11aff25c1b79514ed8cb1c530c4
gonemaul <wongdesoo366@gmail.com>
2024-07-26 09:46:16 +0700

    API Update

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
@@ -31,5 +32,25 @@ class BookController extends Controller
 
     public function show(Book $book){
         return response()->json($book);
+    }
+
+    public function update(Book $book,Request $request){
+        $validatedData = $request->validate([
+            'cover_image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            'name' => ['required', 'string', 'max:255'],
+            'author' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'is_published' => ['required', 'boolean'],
+        ]);
+
+        if ($request->hasFile('cover_image')){
+            Storage::delete($book->cover_image);
+
+            $validatedData['cover_image'] = $request->file('cover_image')->store('images', 'public');
+        }
+
+        $book->update($validatedData);
+
+        return response()->json(['success' => 'Updated book successfully']);
     }
 }
